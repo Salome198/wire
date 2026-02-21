c6d7c464ac2eb44498a549d65b721abe8ea9b424
Salome Madubuko <s.madubuko@wlv.ac.uk>
2026-02-21 17:22:30 +0000

    Created the routes for the login and register through  auth page in controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,12 @@ $routes->get('partners', 'Pages::partners');
 $routes->get('login', 'Auth::login');
 $routes->get('register', 'Auth::register');
 $routes->get('dbtest', 'Dbtest::index');
+$routes->get('login', 'Auth::login');
+$routes->post('login', 'Auth::loginPost');
+
+$routes->get('register', 'Auth::register');
+$routes->post('register', 'Auth::registerPost');
+
+$routes->get('logout', 'Auth::logout');
+
+$routes->get('dashboard', 'Dashboard::index');
